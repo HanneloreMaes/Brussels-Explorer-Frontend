f83b26609c58c7adb1b951b66cbe37fcc0fdd9d8
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-15 14:55:34 +0200

    refactor(App.tsx): testing i18n

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect } from 'react';
 
+import { useTranslation } from 'react-i18next';
 import { Text } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 
+import '@/utils/i18n/i18n';
+
 const App = () => {
+	const { i18n } = useTranslation();
+
 	useEffect(() => {
 		SplashScreen.hide();
 	}, []);
 
 	return(
-		<Text>Hellow</Text>
+		<Text>{i18n.t('routes_label_searchPage')}</Text>
 	);
 };
 
