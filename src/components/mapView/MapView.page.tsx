ffb5aacceaa4c1760b1b30c6d1b7f5d871c64e12
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-16 20:04:11 +0200

    feat: MapView page

---
+++ src/components/mapView/MapView.page.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react';
+
+import MapboxGL from '@rnmapbox/maps';
+import { Text } from 'react-native';
+
+import { MapboxAccesToken } from '@/config';
+
+MapboxGL.setWellKnownTileServer('Mapbox');
+MapboxGL.setAccessToken(MapboxAccesToken);
+
+export const MapView: FC = () => {
+
+	const coordinates = [ 4.3202331, 50.8422424 ];
+
+	return (
+		<MapboxGL.MapView style={{ flex: 1 }}>
+			<MapboxGL.Camera zoomLevel={12} centerCoordinate={coordinates} />
+		</MapboxGL.MapView>
+	);
+};
