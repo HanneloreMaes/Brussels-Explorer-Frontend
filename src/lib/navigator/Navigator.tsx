141a5b98ebd0c5b51bd8a1131b61134fa7919d3e
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-16 15:27:29 +0200

    chore(Navigator): ref to NavigationContainer

--- src/lib/navigator/Navigator.tsx
+++ src/lib/navigator/Navigator.tsx
@@ -3,12 +3,13 @@ import React, { FC } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+import { navigationRef } from '../rootNavigator/RootNavigator';
 import { APP_ROUTES } from '@/enums/routes';
 
 const RootStack = createNativeStackNavigator();
 
 export const Navigator: FC = () => (
-	<NavigationContainer>
+	<NavigationContainer ref={navigationRef}>
 		<RootStack.Navigator
 			screenOptions={{ headerShown: false }}
 		>
