f6f3dd516fe564ec8b30ee2083261072ac25e789
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-16 11:31:06 +0200

    remove(DashboardNavProps): detailpage typecheck

--- src/lib/navigator/types/dashboardNavProps/DashboardNavProp.ts
+++ src/lib/navigator/types/dashboardNavProps/DashboardNavProp.ts
@@ -3,7 +3,6 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 export type DashboardParamList = {
 	DashboardScreen: {titleScreen: string} | undefined;
-	DetailPage: {titleScreen: string} | undefined;
 };
 
 export interface DashboardNavProps<T extends keyof DashboardParamList> {
