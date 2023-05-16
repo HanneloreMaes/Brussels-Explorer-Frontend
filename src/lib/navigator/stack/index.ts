c3815d5c9669c6869a58d6fe3b71dfc9b59b82b9
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-16 15:05:48 +0200

    chore(index.ts): settingspage in indexes

--- src/lib/navigator/stack/index.ts
+++ src/lib/navigator/stack/index.ts
@@ -4,3 +4,4 @@ export * from './onboardingStack/OnboardingStack.navigator';
 // MAIN
 export * from './mainStack/MainStack.navigator';
 export * from './mainStack/dashboardStack/DashboardStack.navigator';
+export * from './mainStack/settingStack/SettingsStack.navigator';
