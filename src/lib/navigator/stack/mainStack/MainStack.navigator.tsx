d21762480090fbdac273c42b6b9af179b991f352
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-16 13:48:18 +0200

    chore(MainStack.navigator): Tabbar in mainStack

--- src/lib/navigator/stack/mainStack/MainStack.navigator.tsx
+++ src/lib/navigator/stack/mainStack/MainStack.navigator.tsx
@@ -2,18 +2,18 @@ import React, { FC } from 'react';
 
 import { BottomTabBarProps, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// import { MainStackTabBar } from '../../../tabBars';
+import { MainStackTabBar } from '../../../tabBars';
 import { MainStackParamList } from '../../types';
 import { MAIN_ROUTES } from '@/enums/routes';
 
 const MainStack = createBottomTabNavigator<MainStackParamList>();
-// const MainTabBar = (props: BottomTabBarProps) => <MainStackTabBar {...props} />;
+const MainTabBar = (props: BottomTabBarProps) => <MainStackTabBar {...props} />;
 
 export const MainStackScreen: FC = () => (
 	<MainStack.Navigator
 		initialRouteName='DashboardStack'
 		screenOptions={{ headerShown: false, unmountOnBlur: true }}
-		// tabBar={MainTabBar}
+		tabBar={MainTabBar}
 	>
 		{MAIN_ROUTES.map((mainItem) => (
 			<MainStack.Screen
