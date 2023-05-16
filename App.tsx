16d37e03bf700217cbc30a91d79d7570dba9aa49
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-16 14:50:04 +0200

    chore(App.tsx) import of FC from react

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useEffect } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { View, StyleSheet, StatusBar } from 'react-native';
