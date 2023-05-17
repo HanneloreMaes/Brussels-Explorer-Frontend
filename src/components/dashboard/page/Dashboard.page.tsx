6c5d88bae3151fb7a87b0d297632bf6c8a6fe96d
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-17 13:56:59 +0200

    chore(Dashboard): RecommendedRoutes component

--- src/components/dashboard/page/Dashboard.page.tsx
+++ src/components/dashboard/page/Dashboard.page.tsx
@@ -1,11 +1,11 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import { useTranslation } from 'react-i18next';
 import { ScrollView, View } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { DashboardStyles } from './Dashboard.styles';
-import { Header, PopularRoutes } from '../components';
+import { Header, RecommendedRoutes } from '../components';
 import { LoadingSpinner } from '@/components/shared';
 import { Highlight } from '@/style';
 import { getRoutes } from '@/utils/redux/Actions';
@@ -13,33 +13,32 @@ import '@/utils/i18n/i18n';
 
 export const DashboardScreen: FC = () => {
 
-	const [ isLoading, setIsLoading ] = useState(true);
 	const { i18n } = useTranslation();
 	const dispatch = useDispatch();
 	const { routes } = useSelector( (state: any) => state.allReducer );
 
 	const fetchRoutes = () => {
 		dispatch(getRoutes());
-		setIsLoading(false);
 	};
 
 	useEffect(() => {
 		fetchRoutes();
-		console.log('Routes', routes);
 	}, []);
 
 	return(
 		<ScrollView style={DashboardStyles.scrollContainer}>
 			<Header />
-			{ isLoading ? (
-				<View style={DashboardStyles.loadingContainer}>
-					<LoadingSpinner sizeSpinner='large' colorSpinner={Highlight.tealHighlight} />
-				</View>
-			) : (
-				<View style={DashboardStyles.marginContainer}>
-					{/* <PopularRoutes data={routes.routes} translation={i18n} /> */}
-				</View>
-			) }
+			{
+				routes.length === 0 ? (
+					<View style={DashboardStyles.loadingContainer}>
+						<LoadingSpinner sizeSpinner='large' colorSpinner={Highlight.tealHighlight} />
+					</View>
+				) : (
+					<View style={DashboardStyles.marginContainer}>
+						<RecommendedRoutes data={routes} translation={i18n} />
+					</View>
+				)
+			}
 		</ScrollView>
 	);
 };
