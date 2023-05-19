3608952381355a88b5d767652125a739e73f3198
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-19 19:29:16 +0200

    feat: Name of mode in store

--- src/utils/redux/Actions.types.ts
+++ src/utils/redux/Actions.types.ts
@@ -1,15 +1,10 @@
-import { Action } from 'redux';
-
 export type Languages = 'nl' | 'en';
 
-export interface RootAction extends Action {
-	points?: any;
-	routes?: any;
-	activeLanguage?: Languages;
-}
+export type ModeOptions = 'light' | 'dark';
 
 // GET-ROUTES => GETTING THE DATA
 export const GET_POINTS = 'GET_POINTS';
 export const GET_ROUTES = 'GET_ROUTES';
 export const GET_POINTS_SPEC_ROUTES = 'GET_POINTS_SPEC_ROUTES';
 export const SET_LANGUAGE = 'SET_LANGUAGE';
+export const SET_MODE_APP = 'SET_MODE_APP';
