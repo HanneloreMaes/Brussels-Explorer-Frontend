4a7d4b1ec18d6121fd2a7501b53a79ac1bddb99b
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-20 12:03:57 +0200

    chore(Navigator): theme depending on mode

--- src/lib/navigator/Navigator.tsx
+++ src/lib/navigator/Navigator.tsx
@@ -1,34 +1,50 @@
 import React, { FC } from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
+import { DarkTheme, DefaultTheme, NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useSelector } from 'react-redux';
 
 import { navigationRef } from '../rootNavigator/RootNavigator';
 import { APP_ROUTES } from '@/enums/routes';
-import { BackgroundColor } from '@/style';
+import { BackgroundColor, TextColor } from '@/style';
 
 const RootStack = createNativeStackNavigator();
 
-const navTheme = {
-	colors: {
-		background: BackgroundColor.light
-	}
-};
+export const Navigator: FC = () => {
 
-export const Navigator: FC = () => (
-	<NavigationContainer ref={navigationRef} theme={navTheme}>
-		<RootStack.Navigator
-			screenOptions={{ headerShown: false }}
-		>
-			{
-				APP_ROUTES.map((navigatorItem) => (
-					<RootStack.Screen
-						key={navigatorItem.name}
-						name={navigatorItem.name}
-						component={navigatorItem.component}
-					/>
-				))
-			}
-		</RootStack.Navigator>
-	</NavigationContainer>
-);
+	const { nameMode } = useSelector((state: any) => state.allReducer);
+
+	const lightTheme = {
+		...DefaultTheme,
+		colors: {
+			...DefaultTheme.colors,
+			background: BackgroundColor.light,
+			text: TextColor.darkText,
+		}
+	};
+	const darkTheme = {
+		...DarkTheme,
+		colors: {
+			...DarkTheme.colors,
+			background: BackgroundColor.dark,
+			text: TextColor.lightText,
+		}
+	};
+	return (
+		<NavigationContainer ref={navigationRef} theme={nameMode === 'light' ? darkTheme : lightTheme}>
+			<RootStack.Navigator
+				screenOptions={{ headerShown: false }}
+			>
+				{
+					APP_ROUTES.map((navigatorItem) => (
+						<RootStack.Screen
+							key={navigatorItem.name}
+							name={navigatorItem.name}
+							component={navigatorItem.component}
+						/>
+					))
+				}
+			</RootStack.Navigator>
+		</NavigationContainer>
+	);
+};
