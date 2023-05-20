bca3fd095011a77380cb94b3a9d33d15c6bb3813
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-20 14:26:56 +0200

    chore(MapView): setting firstCoordinate of point route

--- src/components/mapView/MapView.page.tsx
+++ src/components/mapView/MapView.page.tsx
@@ -1,19 +1,42 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import MapboxGL from '@rnmapbox/maps';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MapboxAccesToken } from '@/config';
+import { getPointsFromSpecRoutes } from '@/utils/redux/Actions';
 
 MapboxGL.setWellKnownTileServer('Mapbox');
 MapboxGL.setAccessToken(MapboxAccesToken);
 
 export const MapView: FC = () => {
 
-	const coordinates = [ 4.3202331, 50.8422424 ];
+	const coordinates = [ 4.3570964, 50.845504 ];
+	const [ firstCoordinateLng, setFirstCoordinateLng ] = useState<number>(0);
+	const [ firstCoordinateLat, setFirstCoordinateLat ] = useState<number>(0);
+
+	const dispatch = useDispatch();
+	const { routes, pointsForSpecRoute } = useSelector((state: any) => state.allReducer);
+
+	const mapRoutes = () => {
+		routes.map((routeDetail: any) => {
+			return fetchSpecRoute(routeDetail._id);
+		});
+		setFirstCoordinateLng(pointsForSpecRoute[ 0 ].lng);
+		setFirstCoordinateLat(pointsForSpecRoute[ 0 ].lat);
+	};
+	const fetchSpecRoute = (idRoute: string) => {
+		dispatch(getPointsFromSpecRoutes(idRoute));
+	};
+
+	useEffect(() => {
+		mapRoutes();
+	}, []);
 
 	return (
 		<MapboxGL.MapView style={{ flex: 1 }}>
-			<MapboxGL.Camera zoomLevel={12} centerCoordinate={coordinates} />
+			<MapboxGL.Camera zoomLevel={12} centerCoordinate={coordinates} animationMode='none' />
+			<MapboxGL.PointAnnotation id="point" coordinate={[ firstCoordinateLng, firstCoordinateLat ]} />
 		</MapboxGL.MapView>
 	);
 };
