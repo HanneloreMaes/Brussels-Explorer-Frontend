4be932c61b4a6db838b1751f7dbcdd97f28b9071
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-23 17:29:02 +0200

    chre(MapViewStack): using i18n translationstrings

--- src/lib/navigator/stack/mainStack/mapviewStack/MapviewStack.navigator.tsx
+++ src/lib/navigator/stack/mainStack/mapviewStack/MapviewStack.navigator.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from 'react';
 
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { useTranslation } from 'react-i18next';
 
 import { MapView } from '@/components';
 import { MAPVIEW_ROUTES } from '@/enums/routes';
 import { MapviewStackParamList } from '@/lib/navigator/types';
 import { MapTopBar } from '@/lib/tabBars/topTabBarMapView/TopBarMapView';
+import '@/utils/i18n/i18n';
 
 const MapOfTopBar = (props: any) => <MapTopBar {...props} />;
 const MapviewStack = createMaterialTopTabNavigator<MapviewStackParamList>();
 
 export const MapviewStackScreen: FC = () => {
+
+	const { i18n } = useTranslation();
 
 	return(
 		<MapviewStack.Navigator
@@ -22,6 +26,9 @@ export const MapviewStackScreen: FC = () => {
 						key={mapItem.label}
 						name={mapItem.name as keyof MapviewStackParamList}
 						component={mapItem.component}
+						options={{
+							title: i18n.t(mapItem.label as string) as string,
+						}}
 						initialParams={{
 							showInHeader: mapItem.showHeader,
 							showInTopBar: mapItem.showTopTab,
