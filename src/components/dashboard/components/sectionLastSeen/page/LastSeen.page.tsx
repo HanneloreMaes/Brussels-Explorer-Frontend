5bd0baf97e266c14a47480476c8ab825452989df
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-27 11:46:19 +0200

    feat(LastSeen): getting last seen route

--- src/components/dashboard/components/sectionLastSeen/page/LastSeen.page.tsx
+++ src/components/dashboard/components/sectionLastSeen/page/LastSeen.page.tsx
@@ -1,24 +1,26 @@
 import React, { FC, useEffect, useState } from 'react';
 
-import { Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import { Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { useSelector } from 'react-redux';
 
 import { LastStyles } from './LastSeen.styles';
 import { RecommendedStyles } from '../../sectionRecRoutes/page/RecommendedSection.styles';
 import { LastSeenType } from '../types/LastSeen.types';
 import * as RootNavigation from '@/lib/rootNavigator/RootNavigator';
 import { TextColor } from '@/style';
-import { getSpecRoute } from '@/utils/redux/Actions';
 
 export const LastSeenSection: FC <LastSeenType> = ({ translation, mode }) => {
 
-	const dispatch = useDispatch();
+	const { specRoute, nameMode } = useSelector((state: any) => state.allReducer);
 
 	const [ noRecent, setNoRecent ] = useState<boolean>(false);
-	const { lastSeen, nameMode, specRoute } = useSelector((state: any) => state.allReducer);
 
 	const getSpecRouteFromId = () => {
-		dispatch(getSpecRoute(lastSeen));
+		if (specRoute) {
+			setNoRecent(false);
+		} else {
+			setNoRecent(true);
+		}
 	};
 
 	useEffect(() => {
@@ -27,40 +29,35 @@ export const LastSeenSection: FC <LastSeenType> = ({ translation, mode }) => {
 
 	return(
 		<View style={LastStyles.container}>
-			<Text style={[ LastStyles.title, { color: mode === 'dark' ? TextColor.lightText : TextColor.darkText } ]} >{translation.t('dashboard_section_last_seen_title')}</Text>
-			<ScrollView horizontal style={RecommendedStyles.allDataContainer}>
-				{/* {
-					noRecent ? <Text style={{ color: nameMode === 'dark' ? TextColor.lightText : TextColor.darkText }}>No recent were found</Text> :
-						data.map((recommendedRoute: any) => {
-							return(
-								<TouchableOpacity key={recommendedRoute._id} style={RecommendedStyles.dataContainer} onPress={() => RootNavigation.navigate('DetailPage', { dataOfCard: recommendedRoute })}>
-									<Image source={{ uri: recommendedRoute.imageUrl }} style={RecommendedStyles.imageRoute} resizeMode='cover' />
-									<Text
-										style={[ RecommendedStyles.nameRoute, { color: mode === 'dark' ? TextColor.lightText : TextColor.darkText } ]}
-									>
-										{recommendedRoute.name}
-										<Text style={RecommendedStyles.infoRoute}> - {recommendedRoute.theme}</Text>
-									</Text>
-								</TouchableOpacity>
-							);
-						})
-				} */}
-				{
-					specRoute.map((last: any) => {
-						return(
-							<TouchableOpacity key={last._id} style={RecommendedStyles.dataContainer} onPress={() => RootNavigation.navigate('DetailPage', { dataOfCard: last })}>
-								<Image source={{ uri: last.imageUrl }} style={RecommendedStyles.imageRoute} resizeMode='cover' />
-								<Text
-									style={[ RecommendedStyles.nameRoute, { color: mode === 'dark' ? TextColor.lightText : TextColor.darkText } ]}
-								>
-									{last.name}
-									<Text style={RecommendedStyles.infoRoute}> - {last.theme}</Text>
-								</Text>
-							</TouchableOpacity>
-						);
-					})
-				}
-			</ScrollView>
+			<Text
+				style={[
+					LastStyles.title,
+					{ color: mode === 'dark' ? TextColor.lightText : TextColor.darkText }
+				]}
+			>{translation.t('dashboard_section_last_seen_title')}</Text>
+			{
+				noRecent ? <Text style={{ color: nameMode === 'dark' ? TextColor.lightText : TextColor.darkText }}>No recent were found</Text> :
+					<ScrollView horizontal style={RecommendedStyles.allDataContainer}>
+						<TouchableOpacity
+							key={specRoute._id}
+							style={RecommendedStyles.dataContainer}
+							onPress={() => RootNavigation.navigate('DetailPage', { dataOfCard: specRoute })}
+						>
+							<Image source={{ uri: specRoute.imageUrl }} style={RecommendedStyles.imageRoute} resizeMode='cover' />
+							<Text
+								style={[
+									RecommendedStyles.nameRoute,
+									{ color: mode === 'dark' ? TextColor.lightText : TextColor.darkText }
+								]}
+							>
+								{specRoute.name}
+							</Text>
+							<View style={RecommendedStyles.bottomThemeContainer}>
+								<Text style={RecommendedStyles.infoRoute}>{specRoute.theme}</Text>
+							</View>
+						</TouchableOpacity>
+					</ScrollView>
+			}
 		</View>
 	);
 };
--- src/components/dashboard/components/sectionLastSeen/page/LastSeen.styles.ts
+++ src/components/dashboard/components/sectionLastSeen/page/LastSeen.styles.ts
@@ -4,7 +4,7 @@ import { DefaultMargins, TextStyles } from '@/style';
 
 export const LastStyles = StyleSheet.create({
 	container: {
-		height: Dimensions.get('window').height/4 + 20,
+		height: Dimensions.get('window').height/3,
 	},
 	title: {
 		...TextStyles.titleH2,
