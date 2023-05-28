a85f2cb361eee247ac233df1dc927730b8f6395d
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-05-28 07:22:59 +0200

    chore(LastSeen.styles): more height to container

--- src/components/dashboard/components/sectionLastSeen/page/LastSeen.styles.ts
+++ src/components/dashboard/components/sectionLastSeen/page/LastSeen.styles.ts
@@ -4,7 +4,7 @@ import { DefaultMargins, TextStyles } from '@/style';
 
 export const LastStyles = StyleSheet.create({
 	container: {
-		height: Dimensions.get('window').height/3,
+		height: Dimensions.get('window').height/3 + 50,
 	},
 	title: {
 		...TextStyles.titleH2,
