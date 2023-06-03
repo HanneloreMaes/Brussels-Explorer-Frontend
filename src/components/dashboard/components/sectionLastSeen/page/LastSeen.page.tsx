cb1e3a52f7f0c161e89b8048509aa6b04ca5d1e6
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-06-03 13:03:13 +0200

    refactor(LastSeen): type for Navigation + usage of  ItemOverview

--- src/components/dashboard/components/sectionLastSeen/page/LastSeen.page.tsx
+++ src/components/dashboard/components/sectionLastSeen/page/LastSeen.page.tsx
@@ -1,16 +1,15 @@
 import React, { FC, useEffect, useState } from 'react';
 
-import { Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { LastStyles } from './LastSeen.styles';
-import { RecommendedStyles } from '../../sectionRecRoutes/page/RecommendedSection.styles';
 import { LastSeenType } from '../types/LastSeen.types';
+import { ItemOverview } from '@/components/searchPage/components';
 import { TitleH2 } from '@/components/shared';
-import * as RootNavigation from '@/lib/rootNavigator/RootNavigator';
 import { TextColor } from '@/style';
 
-export const LastSeenSection: FC <LastSeenType> = ({ mode, dataUnAuth }) => {
+export const LastSeenSection: FC <LastSeenType> = ({ dataUnAuth, navigation }) => {
 
 	const { specRoute, nameMode } = useSelector((state: any) => state.allReducer);
 
@@ -35,47 +34,23 @@ export const LastSeenSection: FC <LastSeenType> = ({ mode, dataUnAuth }) => {
 				noRecent ? <Text style={{ color: nameMode === 'dark' ? TextColor.lightText : TextColor.darkText }}>No recent were found</Text> :
 					(
 						dataUnAuth === undefined ? (
-							<ScrollView horizontal style={RecommendedStyles.allDataContainer}>
-								<TouchableOpacity
-									key={specRoute._id}
-									style={RecommendedStyles.dataContainer}
-									onPress={() => RootNavigation.navigate('DetailPage', { dataOfCard: specRoute })}
-								>
-									<Image source={{ uri: specRoute.imageUrl }} style={RecommendedStyles.imageRoute} resizeMode='cover' />
-									<Text
-										style={[
-											RecommendedStyles.nameRoute,
-											{ color: mode === 'dark' ? TextColor.lightText : TextColor.darkText }
-										]}
-									>
-										{specRoute.name}
-									</Text>
-									<View style={RecommendedStyles.bottomThemeContainer}>
-										<Text style={RecommendedStyles.infoRoute}>{specRoute.theme}</Text>
-									</View>
-								</TouchableOpacity>
-							</ScrollView>
+							<ItemOverview
+								key={specRoute.name}
+								item={specRoute}
+								nameMode={nameMode}
+								navigation={navigation}
+								prevPage='Last'
+							/>
 						) : (
 							dataUnAuth.map((unAuth: any) => {
 								return (
-									<TouchableOpacity
+									<ItemOverview
 										key={unAuth._id}
-										style={RecommendedStyles.dataContainer}
-										onPress={() => RootNavigation.navigate('DetailPage', { dataOfCard: unAuth })}
-									>
-										<Image source={{ uri: specRoute.imageUrl }} style={RecommendedStyles.imageRoute} resizeMode='cover' />
-										<Text
-											style={[
-												RecommendedStyles.nameRoute,
-												{ color: mode === 'dark' ? TextColor.lightText : TextColor.darkText }
-											]}
-										>
-											{unAuth.name}
-										</Text>
-										<View style={LastStyles.bottomContainer}>
-											<Text style={RecommendedStyles.infoRoute}>{unAuth.theme}</Text>
-										</View>
-									</TouchableOpacity>
+										item={unAuth}
+										nameMode={nameMode}
+										navigation={navigation}
+										prevPage='Last'
+									/>
 								);
 							})
 						)
