8277b545481522d3db939d5da0f5316fed938904
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-06-05 10:35:26 +0200

    chore(TitleH2): set right width for container

--- src/components/shared/titleH2/TitleH2.page.tsx
+++ src/components/shared/titleH2/TitleH2.page.tsx
@@ -1,40 +1,56 @@
 import React, { FC, useEffect, useState } from 'react';
 
 import { useTranslation } from 'react-i18next';
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions, PixelRatio } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { TitleType } from './TitleH2.types';
 import { RecommendedStyles } from '@/components/dashboard/components/sectionRecRoutes/page/RecommendedSection.styles';
+import { BARRIER_SCREEN } from '@/config';
 import { UnderlineStyle, TextColor } from '@/style';
 import '@/utils/i18n/i18n';
 
+const { width } = Dimensions.get('window');
+
 export const TitleH2: FC<TitleType> = ({ labelTitle, prevComponent }) => {
 
+	const screenRation = Dimensions.get('window').width * PixelRatio.get();
 	const { i18n } = useTranslation();
 	const { nameMode } = useSelector((state: any) => state.allReducer);
 
-	const [ widthContainerRec, setWidthContainerRec ] = useState<string>('70%');
-	const [ widthContainerLast, setWidthContainerLast ] = useState<string>('29%');
+	const [ widthContainerRec, setWidthContainerRec ] = useState<number>(width * 0.73);
+	const [ widthContainerLast, setWidthContainerLast ] = useState<number>(width * 0.4);
 
 	const checkLanguageWidth = () => {
 		const lang = i18n.language;
 
-		if (lang === 'en') {
-			setWidthContainerRec('74.5%');
-			setWidthContainerLast('32%');
+		if (lang === 'en' && screenRation > BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.73);
+			setWidthContainerLast(width * 0.32);
+		} else if (lang === 'en' && screenRation < BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.64);
+			setWidthContainerLast(width * 0.27);
 		}
-		if (lang === 'fr') {
-			setWidthContainerRec('75%');
-			setWidthContainerLast('65.5%');
+		if (lang === 'nl' && screenRation > BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.63);
+			setWidthContainerLast(width * 0.5);
+		} else if (lang === 'nl' && screenRation < BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.55);
+			setWidthContainerLast(width * 0.45);
 		}
-		if (lang === 'nl') {
-			setWidthContainerRec('63%');
-			setWidthContainerLast('50%');
+		if (lang === 'fr' && screenRation > BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.8);
+			setWidthContainerLast(width * 0.7);
+		} else if (lang === 'fr' && screenRation < BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.7);
+			setWidthContainerLast(width * 0.6);
 		}
-		if (lang === 'de') {
-			setWidthContainerRec('65%');
-			setWidthContainerLast('53%');
+		if (lang === 'de' && screenRation > BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.65);
+			setWidthContainerLast(width * 0.53);
+		} else if (lang === 'de' && screenRation < BARRIER_SCREEN) {
+			setWidthContainerRec(width * 0.57);
+			setWidthContainerLast(width * 0.45);
 		}
 	};
 
