8b3ae96d02b1ba2f0d0a6639404c257b0a8d3c38
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-06-06 19:34:34 +0200

    refactor: position of ModalError -> to folder modals

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -4,9 +4,10 @@ export * from './detailPointPage/page/DetailPoint.page';
 export * from './detailPage/page/Detail.page';
 export * from './detailPage/page/Detail.styles';
 export * from './detailPage/components/detailMap/DetailMap.styles';
-export * from './modalErrors/ModalError.page';
+export * from './modals/modalErrors/ModalError.page';
 export * from './titleH2/TitleH2.page';
 export * from './registerModal/RegisterModal.page';
+export * from './modals/firebaseModal/FirebaseModal.page';
 
 export * from './detailPage/components/descriptionDetail/DescriptionDetail.page';
 export * from './detailPage/components/pointsDetail/PointsDetail.page';
