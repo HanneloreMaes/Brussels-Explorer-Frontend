136632664285fd11281bcce7e19292a6c20a48af
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-06-06 11:31:41 +0200

    chore(TitleOnboarding): alignItem center

--- src/components/onboarding/components/title/TitleOnboarding.page.tsx
+++ src/components/onboarding/components/title/TitleOnboarding.page.tsx
@@ -10,7 +10,7 @@ const { width, height } = Dimensions.get('window');
 
 export const TitleOnboarding: FC <TitleOnboardingType> = ({ nameMode }) => {
 	return (
-		<View style={{ zIndex: 1 }}>
+		<View style={{ zIndex: 1, alignItems: 'center' }}>
 			<Image source={require('@/assets/logo/ic_logo.png')} style={{ width: width * 0.4, height: height * 0.21, marginBottom: 10, }} />
 			<Text
 				style={[
