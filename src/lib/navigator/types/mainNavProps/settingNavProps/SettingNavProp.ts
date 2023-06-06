cfa143f53518b7bd27f58daf3154daf91c80248c
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-06-06 16:20:17 +0200

    chore: settig UsernameSetting in routes + navProps

--- src/lib/navigator/types/mainNavProps/settingNavProps/SettingNavProp.ts
+++ src/lib/navigator/types/mainNavProps/settingNavProps/SettingNavProp.ts
@@ -4,6 +4,7 @@ import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 export type SettingStackParamList = {
 	Settings: {titleScreen: string} | undefined;
 	Language: {titleScreen: string} | undefined;
+	UsernameSetting: {titleScreen: string} | undefined;
 	Onboarding: undefined;
 };
 
