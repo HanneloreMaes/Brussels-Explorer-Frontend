dd99b989dc74885ba0e7f8f7b2e3f70ab768ffb4
HanneloreMaes <hannelore.maes@student.ehb.be>
2023-06-10 11:35:16 +0200

    chore(DetailMap.page): refactor params from props + View for expand buttons

--- src/components/shared/detailPage/components/detailMap/DetailMap.tsx
+++ src/components/shared/detailPage/components/detailMap/DetailMap.tsx
@@ -1,23 +1,21 @@
 import React, { FC, useEffect, useState } from 'react';
 
 import MapboxGL, { CircleLayerStyle,SymbolLayerStyle } from '@rnmapbox/maps';
-import { Image, View, Text, Dimensions } from 'react-native';
+import { View, Text } from 'react-native';
+import FontAwsome5 from 'react-native-vector-icons/FontAwesome5';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { DetailMapStyles } from './DetailMap.styles';
 import { DetailMapTypes } from './DetailMap.types';
 import { ModalError } from '../../..';
-import { MapStyles } from '@/components/mapView/MapView.styles';
-import { MapboxAccesToken } from '@/config';
-import { BackgroundColor, BorderContainerStyle, DefaultAppStyling, DefaultShadow, Highlight, TextColor } from '@/style';
+import { MapboxAccesToken, screenHeight } from '@/config';
+import { BackgroundColor, DefaultShadow, Highlight, TextColor } from '@/style';
 import { getPointsFromSpecRoutes } from '@/utils/redux/Actions';
 
 MapboxGL.setWellKnownTileServer('Mapbox');
 MapboxGL.setAccessToken(MapboxAccesToken);
 
-const { height } = Dimensions.get('window');
-
-export const DetailMap: FC <DetailMapTypes> = ({ dataRoute }) => {
+export const DetailMap: FC = (props: any) => {
 
 	const [ centerCo, setCenterCo ] = useState();
 	const [ routeGeo, setRouteGeo ] = useState<any>();
@@ -27,7 +25,7 @@ export const DetailMap: FC <DetailMapTypes> = ({ dataRoute }) => {
 	const [ showName, setShowName ] = useState<boolean>(false);
 	const [ namePoint, setNamePoint ] = useState<string>('');
 
-	const routeId = dataRoute._id;
+	const routeId = props?.dataRoute._id;
 
 	const dispatch = useDispatch();
 	const { pointsForSpecRoute, nameMode } = useSelector((state: any) => state.allReducer);
@@ -115,10 +113,12 @@ export const DetailMap: FC <DetailMapTypes> = ({ dataRoute }) => {
 
 	useEffect(() => {
 		fetchPointsSpecRoute();
-	},[ dataRoute ]);
+	},[ props?.dataRoute ]);
 
 	return (
-		<View style={DetailMapStyles.mapContainer}>
+		<View style={
+			props?.scaleBig === true ? DetailMapStyles.mapContainerBig : DetailMapStyles.mapContainer
+		}>
 			<MapboxGL.MapView
 				style={{ flex: 1 }}
 				styleURL='mapbox://styles/mapbox/streets-v12'
@@ -157,6 +157,33 @@ export const DetailMap: FC <DetailMapTypes> = ({ dataRoute }) => {
 					showModalError ? <ModalError labelName="mapbox_error_no_routes" labelTryAgainText='mapbox_error_try_again' /> : null
 				}
 			</MapboxGL.MapView>
+			<View
+				style={[
+					DetailMapStyles.scaleMap,
+					DefaultShadow.bottomShadow,
+					{
+						backgroundColor: nameMode === 'dark' ? BackgroundColor.dark : BackgroundColor.light
+					}
+				]}
+			>
+				{
+					props?.scaleBig === true ? (
+						<FontAwsome5
+							name='compress-arrows-alt'
+							size={25}
+							color={nameMode === 'dark' ? TextColor.lightText : TextColor.darkText}
+							onPress={() => props?.handleScaleBigMap(false)}
+						/>
+					) : (
+						<FontAwsome5
+							name='expand-arrows-alt'
+							size={25}
+							color={nameMode === 'dark' ? TextColor.lightText : TextColor.darkText}
+							onPress={() => props?.handleScaleBigMap(true)}
+						/>
+					)
+				}
+			</View>
 			{
 				showName === true ? (
 					<View
